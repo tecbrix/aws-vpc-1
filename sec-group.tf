fae22d5d64279d9d16a8af575b2c3225f11899c3
amitvm <53211276+amitvm@users.noreply.github.com>
2022-04-01 20:27:01 +0530

    Create sec-group.tf

---
+++ sec-group.tf
@@ -0,0 +1,62 @@
+resource aws_security_group "web" {
+  name        = "${var.name}-webSG"
+  description = "This is for ${var.name}s web servers security group"
+  vpc_id      = aws_vpc.Main.id
+
+  tags = {
+    Name = "${var.name}-webSG"
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "icmp"
+    from_port   = -1
+    to_port     = -1
+    cidr_blocks = [aws_vpc.Main.cidr]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource aws_security_group "mysql" {
+  name        = "${var.name}-DBSG"
+  description = "managed by terrafrom for db servers"
+  vpc_id      = aws_vpc.Main.id
+
+  tags = {
+    Name = "${var.name}-DBSG"
+  }
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    security_groups = [aws_security_group.web.id]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
