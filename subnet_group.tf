929eeff5f0a67d5188bb559b410585f37fa42e50
Naveed ur Rahman <naveed.rahman.ext@axa-gulf.com>
2022-08-03 14:48:51 +0400

    Formatting

--- subnet_group.tf
+++ subnet_group.tf
@@ -2,7 +2,7 @@ resource "aws_db_subnet_group" "rds" {
   depends_on = [
     aws_subnet.privatesubnets
   ]
-  name = "rds-subngroup"
+  name = "rds-subnetgroup"
   subnet_ids = [
     for index in aws_subnet.privatesubnets[*].id :
     index
