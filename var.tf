24f196591642c8d59a745b2ab627bd2e35a9ef7d
Muhammad Hanzala <101108492+Hanzala47@users.noreply.github.com>
2023-03-22 13:01:05 +0500

    Update var.tf

--- var.tf
+++ var.tf
@@ -7,12 +7,12 @@ variable "main_vpc_cidr" {
 }
 
 variable "public_subnets" {
-  type    = list(any)
+  type    = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
-  type    = list(any)
+  type    = list(string)
   default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
